1fe68e774884f71dc7f43b9a8d7fc41200ac1187
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-07-03 15:12:27 -0300

    Criando dashboard 1/7

---
+++ dashboard/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat;400,700" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <div class="sidebar">
+        <div class="topo">
+            <h3>Danki.Code</h3>
+        </div>
+        <!-- topo -->
+        <div class="menu">
+            <ul>
+                <li><i class="fas fa-angle-double-right"></i><a href="#"> Home</a></li>
+                <li><i class="fas fa-angle-double-right"></i><a href="#"> Home</a></li>
+                <li><i class="fas fa-angle-double-right"></i><a href="#"> Home</a></li>
+                <li><i class="fas fa-angle-double-right"></i><a href="#"> Home</a></li>
+                <li><i class="fas fa-angle-double-right"></i><a href="#"> Home</a></li>
+            </ul>
+        </div>
+        <!-- menu -->
+    </div>
+    <!-- sidebar -->
+</body>
+</html>
